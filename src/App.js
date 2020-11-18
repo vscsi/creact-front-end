063b4d4cee67a0bd2332e96c6a9cf9a9149394cb
Venus Chan <vchan446@gmail.com>
2020-11-18 13:58:39 +0800

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Welcome to Creact</h1>
+
+    </div>
+  );
+}
+
+export default App;
