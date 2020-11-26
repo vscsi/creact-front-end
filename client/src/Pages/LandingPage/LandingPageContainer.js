c0d59e249493d2b7ea1a111c6cb07770a629d360
wongw859 <wongw859@gmail.com>
2020-11-26 11:45:26 +0800

    created Landing page container

---
+++ client/src/Pages/LandingPage/LandingPageContainer.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import NavBar from "./Nav/NavBar"
+import Download from "./Content/Download/Download";
+import Price from "./Content/Price/Price";
+import Reason from "./Content/Reason/Reason";
+import Safety from "./Content/Safety/Safety";
+import Support from "./Content/Support/Support";
+import Home from "./Content/Home/Home";
+import Login from "./Content/Login/Login";
+
+function LandingPageContainer() {
+  return (
+    <>
+      <Router>
+        <NavBar />
+        <Switch>
+          <Route path="/" exact component={Home} />
+          <Route path="/download" component={Download} />
+          <Route path="/reason" component={Reason} />
+          <Route path="/price" component={Price} />
+          <Route path="/safety" component={Safety} />
+          <Route path="/support" component={Support} />
+          <Route path="/login" component={Login} />
+        </Switch>
+      </Router>
+    </>
+  )
+}
+
+export default LandingPageContainer
