367781a0b9bcf32d692f957ade3bc92ffb67320b
Venus Chan <vchan446@gmail.com>
2020-12-10 17:13:12 +0800

    removed server

--- server/database.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE task(
-id serial PRIMARY KEY,
-task_name VARCHAR(255) NOT NULL,
-task_content VARCHAR(1000) NOT NULL,
-task_deadline timestamp NOT NULL,
-task_user VARCHAR(10)
-);
--- server/routes/register.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const registerController = require('../controllers/register');
-
-router.get('/register',registerController.getRegister)
-router.post('/register',registerController.postRegister)
-
-module.exports=router;
