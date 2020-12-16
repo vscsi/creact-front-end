01b753a4bbc56ef9ae88a7f053260a11ddbc20ca
Venus Chan <vchan446@gmail.com>
2020-12-16 18:38:35 +0800

    fixed warnings

--- src/Pages/DashboardPage/DashboardComponent/DashboardNavbar.js
+++ src/Pages/DashboardPage/DashboardComponent/DashboardNavbar.js
@@ -1,20 +1,20 @@
 import React from 'react'
-import * as MaterialUI from '@material-ui/core';
-import PermIdentityIcon from '@material-ui/icons/PermIdentity';
+// import * as MaterialUI from '@material-ui/core';
+// import PermIdentityIcon from '@material-ui/icons/PermIdentity';
 import DashboardNavbarCss from './DashboardNavbar.module.css'
-import { makeStyles } from '@material-ui/core/styles';
+// import { makeStyles } from '@material-ui/core/styles';
 // import Paper from '@material-ui/core/Paper';
 
 function DashboardNavbar() {
   //overriding Avatar root class
-  const styles =makeStyles(
-    {
-      root: {
-        margin:'0.5rem'
-      }
-    }
-  );
-  const classes = styles();
+  // const styles =makeStyles(
+  //   {
+  //     root: {
+  //       margin:'0.5rem'
+  //     }
+  //   }
+  // );
+  // const classes = styles();
   /////
   return (
     <>
