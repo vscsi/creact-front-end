453de20dae5f369b5246c25fe74e44147fb6435a
Venus Chan <vchan446@gmail.com>
2020-12-18 14:04:09 +0800

    fixedwarnings

--- src/Pages/DashboardPage/DashboardComponent/DashboardNavbar.js
+++ src/Pages/DashboardPage/DashboardComponent/DashboardNavbar.js
@@ -1,34 +1,41 @@
 import React from "react";
+import * as MaterialUI from "@material-ui/core";
 // import PermIdentityIcon from '@material-ui/icons/PermIdentity';
 import DashboardNavbarCss from "./DashboardNavbar.module.css";
+import { makeStyles } from "@material-ui/core/styles";
 
 function DashboardNavbar(props) {
   // const [userName, setUserName] = useState("");
   //overriding Avatar root class
-
+  const styles = makeStyles({
+    root: {
+      margin: "0.5rem",
+    },
+  });
+  const classes = styles();
 
   return (
     <>
       <nav className={DashboardNavbarCss.DashboardNav}>
         <div className={DashboardNavbarCss.remindBox}>Remind Box</div>
 
-        {props.loginUsers.map((item, index) => {return (
-          <div className={DashboardNavbarCss.userIcon}>
-          <MaterialUI.Avatar key={index} classes={{ root: classes.root }}>
-            {item.username}
-          </MaterialUI.Avatar>
+        <div className={DashboardNavbarCss.userIcon}>
+          {props.loginUsers.map((item, index) => {
+            return (
+              <MaterialUI.Avatar key={index} classes={{ root: classes.root }}>
+                {item.username[0]}
+              </MaterialUI.Avatar>
+            );
+          })}
         </div>
-        );
-          
-        })}
         {/* <div className={DashboardNavbarCss.userIcon}>
           <MaterialUI.Avatar classes={{ root: classes.root }}>
             {props.loginUsers[0].name}
           </MaterialUI.Avatar>
-        </div> 
-        {props.loginUsers.map((item, index) => {
+        </div> /}
+        {/ {props.loginUsers.map((item, index) => {
           return <span key={index}>{item.username}</span>;
-        })}*/}
+        })} */}
       </nav>
     </>
   );
